7de9a157d7b0005812c80a73f92cb22c374769b1
ota-meshi <otameshiyo23@gmail.com>
2016-01-16 22:50:01 +0900

    page

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,8 @@
-console.log('This would be the main JS file.');
+(function() {
+	'use strict';
+	window.onload = function(e) {
+		var edit = document.getElementById('edit');
+
+		window.visiblespaces.edit(edit);
+	};
+})();
